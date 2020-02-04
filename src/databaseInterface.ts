f7d17bb0227dbae5e6c8cdbafc2c46cbb48dbbca
Martin Bradley <mbrad@home.com>
2020-02-04 22:48:30 +0000

    Checks a database

--- src/databaseInterface.ts
+++ src/databaseInterface.ts
@@ -1,18 +1,20 @@
-//const { Pool } = require('pg')
+const { Pool } = require('pg')
 
 export default function doit() {
    console.log("hi therre from database");
 
-//  const pool = new Pool({
-//    user: 'pguser',
-//    host: 'localhost',
-//    database: 'pgdatabase',
-//    password: 'pguser1',
-//    port: 5432,
-//  })
+    const pool = new Pool({
+      user: 'pguser',
+      host: 'localhost',
+      database: 'pgdatabase',
+      password: 'pguser1',
+      port: 5432,
+    })
 
-//  pool.query('SELECT NOW()', (err, res) => {
-//    console.log(err, res)
-//    pool.end()
-//  })
+    pool.query('select * from patient', 
+               (err: any, res: any) => {
+                  console.log(err, res)
+                  pool.end()
+               }
+    );
 }
